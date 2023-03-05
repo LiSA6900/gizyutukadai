b24dfa6d23ef621ada552eca436e1ac4a478e468
LiSA6900 <amnos.1999.arasick@gmail.com>
2023-03-05 14:39:40 +0000

    [Add]送信メールの設定

--- confirm.php
+++ confirm.php
@@ -1,17 +1,30 @@
 <?php
-//直リンクされた場合index.htmlにリダイレクト
+$str = mb_convert_encoding($str, "UTF-8", "JIS, eucjp-win, sjis-win");
+
+//直リンクされた場合form.htmlにリダイレクト
 if($_SERVER["REQUEST_METHOD"] != "POST"){
-	header("Location: form.html");
-	exit();
+  header("Location: form.html");
+  exit();
 }
 
-//各項目を内容を取得
-$choice = $_POST['choice'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
-$message = $_POST['message'];
-$upload_file = $_FILES['upload_file'];
+//画像を保存
+// move_uploaded_file($_FILES['image']['tmp_name'], './upload/' . $img_name);
+
+// フォームのボタンが押されたら
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // フォームから送信されたデータを各変数に格納
+  $choice = $_POST["choice"];
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $tel = $_POST["tel"];
+  $message = $_POST["message"];
+  // アップロードファイル名を取得
+  //$image = '';
+  //  if ($_FILES['image']['name']) {
+  //    $image = htmlspecialchars($_FILES['image']['name'], ENT_QUOTES, 'UTF-8');
+  //  }
+}
+
 ?>
 
 <!--PHPでhtmlファイルをincludeして読み込む-->
--- send.php
+++ send.php
@@ -1,77 +1,56 @@
 <?php
-//直リンクされた場合upload.phpにリダイレクト
-if($_SERVER["REQUEST_METHOD"] != "POST"){
-	header("Location: form.html");
-	exit();
-}
-
-// POSTデータの下ごしらえ
-foreach($_POST as $k => $v) {
-	$$k = htmlspecialchars($v);
-}
-
-// アップロードファイル名を取得
-$upload_file = '';
-if ($_FILES['upload_file']['name']) {
-	$upload_file = htmlspecialchars($_FILES['upload_file']['name'], ENT_QUOTES, 'UTF-8');
-}
-
-//メールの日本語設定
-mb_language("Japanese");
+// 送信ボタンが押されたら
+if (isset($_POST["submit"])) {
+// 送信ボタンが押された時に動作する処理をここに記述する
+      
+// 日本語をメールで送る場合のおまじない
+mb_language("Ja");
 mb_internal_encoding("UTF-8");
+  
+//送信先アドレス
+$to = $_POST['email'];
+//メール件名
+$subject = "お問い合わせありがとうございます。";
 
-// ユーザー向けのメール内容の準備
-$body = "--__BOUNDARY__\n";
-$body .= "Content-Type: text/plain; charset=\"ISO-2022-JP\"\n\n";
-$body .= <<<EOT
- 
-お問い合わせをいただき、ありがとうございました。
-折り返し担当者から返信が行きますので、しばらくお待ちください。
-以下の内容でお問い合わせをお受けいたしました。
- 
-< お問い合わせ内容 >
+// メール本文を変数bodyに格納
+$body = <<<EOM
+{$name} 様
 
-■件名
+お問い合わせありがとうございます。
+以下のお問い合わせ内容をメールにて確認させていただきました。
+
+===================================================
+【 件名 】 
 {$choice}
- 
-■お名前
+
+【 お名前 】 
 {$name}
- 
-■メールアドレス
+
+【 メールアドレス 】 
 {$email}
 
-■電話番号
+【 電話番号 】 
 {$tel}
- 
-■お問い合わせ内容
-{$message}
- 
-■添付ファイル
-{$upload_file}
- 
- 
-EOT;
-	
-$body .= "--__BOUNDARY__\n";
-$body .= "Content-Type: application/octet-stream; name=\"{$upload_file}\"\n";
-$body .= "Content-Disposition: attachment; filename=\"{$upload_file}\"\n";
-$body .= "Content-Transfer-Encoding: base64\n";
-$body .= "\n";
-$body .= chunk_split(base64_encode(file_get_contents($_FILES['upload_file']['tmp_name'])));
-$body .= "--__BOUNDARY__\n";
- 
- 
-$from_mail = "sample@test.com"; // 送信元メールアドレス
-$subject = "お問い合わせありがとうございます";
- 
-$header = "Content-Type: multipart/mixed;boundary=\"__BOUNDARY__\"\n";
-$header .= "Return-Path: " . $from_mail . " \n";
-$header .= "From: " . mb_encode_mimeheader($from_encoded) . "<" . $from_mail . ">\n";
-$header .= "Reply-To: ".$from_mail."\n";
- 
-// メール送信
-$success_1 = mb_send_mail($email, $subject, $body, $header);
-?>
 
-<!--PHPでhtmlファイルをincludeして読み込む-->
-<?php include('./complete.html'); ?>
+【 お問合せ内容 】 
+{$message}
+
+【 添付画像 】 
+
+===================================================
+
+内容を確認の上、回答させていただきます。
+しばらくお待ちください。
+EOM;
+
+//送信元
+$headers = "From: example@test.com";
+//メール送信
+mb_send_mail($to, $subject, $body, $headers); 
+
+// サンクスページに画面遷移させる
+header("Location: complete.html");
+exit();
+}
+
+?>
