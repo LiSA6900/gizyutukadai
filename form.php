a2ddac235c5c794e5ddab8aaa65636734c0d78d9
LiSA6900 <amnos.1999.arasick@gmail.com>
2023-03-08 08:32:45 +0000

    [Fix]DB接続

--- form.php
+++ form.php
@@ -1,30 +1,19 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=testdb';
-$username = 'root';
-$password = 'asdfasdf01';
-$driver_options = [
-  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_EMULATE_PREPARES => false,
-];
-$pdo = new PDO($dsn, $username, $password, $driver_options);
+$user = "root";
+$pass = "asdfasdf01";
+$dsn = "mysql:dbname=testdb;host=localhost;charset=utf8";
 
-try {
-  $dbh = new PDO($dsn, $username, $password,
-  array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) );
-  if ($dbh == null) {
-    print_r('接続失敗').PHP_EOL; 
-  } else {  
-    print_r('接続成功').PHP_EOL;
-  }
-} catch(PDOException $e) {
-  echo('Connection failed:'.$e->getMessage());
-  die();
-}
-$sql = 'SHOW TABLES';
-$stmt = $dbh->query($sql);
-while ($result = $stmt->fetch(PDO::FETCH_NUM)){
-  $choices[] = $result[0];
+try{
+  $dbh = new PDO($dsn, $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql="SELECT choice FROM `choice_data`";
+  $stmt = $dbh->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  print_r($result);
+  $dbh = null;
+}      
+catch(PDOException $e){
+  echo 'DB接続エラー'.$e->getMessage();
 }
 
 include('./form.html');
